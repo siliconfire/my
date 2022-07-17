89f387e9270e2a0a498cdb52bd96776adc1c03e1
unnamed-file <96432924+Trashbin7@users.noreply.github.com>
2022-07-17 21:26:59 +0300

    Update 1.py

---
+++ 1.py
@@ -0,0 +1,11 @@
+while True:
+    print("YOUTUBE SIMULATOR")
+    print("1. video arama")
+    print("2. video düzenleme")
+    choice = int(input(1 or 2))
+        if choice == 1:
+            print("video aranıyor...")
+            print("bulunamadı!")
+        elif choice == 2:
+            print("video düzenleniyor...") 
+            print("düzenlenemedi!")
