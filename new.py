679031652d204b8887742b98ed259c75f6dc6b07
unnamed-file <96432924+Trashbin7@users.noreply.github.com>
2022-07-20 19:52:32 +0300

    Create new.py

---
+++ new.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+counter = 0
+
+while True:
+    print("----------------------------------------------------")
+    print("Counter:" + str(counter))
+    print("----------------------------------------------------")
+    counter = counter + 1
+    os.system('cls')
