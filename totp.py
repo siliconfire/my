d91594a54661e1d2134e657bf5834eff2803d852
unnamed-file <96432924+Trashbin7@users.noreply.github.com>
2022-07-22 10:03:51 +0300

    2 changed files

---
+++ totp.py
@@ -0,0 +1,10 @@
+import pyotp
+import time
+
+import pyotp
+totp = pyotp.TOTP("JBSWY3DPEHPK3PXP")
+print("----------------------------------------------------")
+print("Mineland için totp kodu:", totp.now())
+print("")
+print("made by github.com/trashbin7")
+print("----------------------------------------------------")
