f90616334566cf91f44452dbf61befb5cb0fdcdc
unnamed-file <96432924+Trashbin7@users.noreply.github.com>
2022-07-27 09:54:47 +0300

    spammer.py changed

--- spammer.py
+++ spammer.py
@@ -1,1 +1,7 @@
+
+
 print("-----------------------------------------------------")
+print("Hoş geldiniz. Bu programın amacı, bir e-posta adresine")
+print("yollanan bir mesajınızın gönderilmesini sağlamaktır.")
+print("-----------------------------------------------------")
+print("")
