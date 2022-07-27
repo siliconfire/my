23236e78c55d72eed2060b931cccb54c16bd9d12
unnamed-file <96432924+Trashbin7@users.noreply.github.com>
2022-07-27 10:02:01 +0300

    Added a warning to spammer.py

--- spammer.py
+++ spammer.py
@@ -1,7 +1,11 @@
-
-
+version = 0.3
+print("WARNING: we are not responsible for any damage caused by this program.")
 print("-----------------------------------------------------")
-print("Hoş geldiniz. Bu programın amacı, bir e-posta adresine")
-print("yollanan bir mesajınızın gönderilmesini sağlamaktır.")
+print("Welcome to text spammer.")
+print("version: " + str(version))
+print("")
+print("made by github.com/trashbin7")
 print("-----------------------------------------------------")
 print("")
+
+
