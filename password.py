87847428da9c7afeb2f9b0e15bb3c488cbffd890
unnamed-file <96432924+Trashbin7@users.noreply.github.com>
2022-07-29 17:50:32 +0300

    added welcome and length to password.py

---
+++ password.py
@@ -0,0 +1,49 @@
+import time        # Import the time module to use the sleep function
+import random      # Import the random module to use the randint function
+import sys         # Import the sys module to use the exit function
+import os          # Import the os module to use the system function
+import keyboard    # Import the keyboard module to use the is_pressed function
+
+os.system('cls' if os.name == 'nt' else 'clear')
+print("-----------------------------------------------------")
+print("Welcome to the password generator!")
+print("This program will generate a random password for you.")
+print("")
+print("(!) press [Enter] or [a] to start.")
+print("")
+print("github.com/trashbin7")
+print("-----------------------------------------------------")
+
+keyboard.wait("enter" or "a")
+time.sleep(0.3)
+os.system('cls' if os.name == 'nt' else 'clear')
+print("-----------------------------------------------------")
+print("Welcome to the password generator!")
+print("This program will generate a random password for you.")
+print("")
+print("")
+print("")
+print("github.com/trashbin7")
+print("-----------------------------------------------------")
+time.sleep(0.3)
+os.system('cls' if os.name == 'nt' else 'clear')
+print("-----------------------------------------------------")
+print("Welcome to the password generator!")
+print("This program will generate a random password for you.")
+print("")
+print("(!) press [Enter] or [a] to start.")
+print("")
+print("github.com/trashbin7")
+print("-----------------------------------------------------")
+time.sleep(0.3)
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
+print("-----------------------------------------------------")
+print("Pick a password length:")
+print("")
+print("[A] 8 characters")
+print("[B] 12 characters")
+print("[C] 16 characters")
+print("")
+print("-----------------------------------------------------")
